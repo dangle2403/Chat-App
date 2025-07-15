110a9cae406616b40a1fe620f2c708e1345ac6af
Dang Le <dang.le.tech2403@gmail.com>
2025-07-15 16:01:10 -0500

    Create message model

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,6 @@ import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -14,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
+app.use("/api/message", authRouter);
 app.use("/api/profile", profileRoutes);
 
 app.listen(PORT, () => {
