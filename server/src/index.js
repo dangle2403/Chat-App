3e3228d66b8be2de87d836554c3811ce82927916
Dang Le <dang.le.tech2403@gmail.com>
2025-07-17 19:52:59 -0500

    feat: create setup for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -7,10 +7,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import messageRouter from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
-
+const __dirname = path.resolve();
 
 
 app.use(express.json({ limit: "10mb" }));
@@ -26,6 +27,15 @@ app.use(
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/profile", profileRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+
+};
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
